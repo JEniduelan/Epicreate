5c8183bc4c95af1f1594de4fdef9588318a95016
JEniduelan <jeniduelan@gmail.com>
2024-04-25 21:42:07 +1000

    added create function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# System Packages
+import csv
+import os
+
+# External Packages
+from colored import Fore, Back, Style
+# App functions
+from functions import create_character
 print("Welcome to Epicreate")
 
 # Menu function
@@ -5,20 +13,23 @@ def menu():
     while True:
         print("\nMain Menu:")
         print("> Enter 1 to create a character ")
-        print("> Enter 2 to delete customised character ")
-        print("> Enter 3 to view customised character ")
+        print("> Enter 2 to delete created character ")
+        print("> Enter 3 to view created character ")
         print("> Enter 4 to exit ")
 
         choice = input("Enter your choice: ")
         return choice
-    
+
+
+csv_file = "Characters.csv"
+
 choice = ""
 
 while choice != "4":
       choice = menu()
 
       if choice == "1":
-            print(1)
+           character_ = create_character()
       elif choice == "2":
             print(2)
       elif choice == "3":
