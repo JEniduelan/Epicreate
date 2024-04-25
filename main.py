31e8d009e46cdb2de6152442e578370999fbe9a8
JEniduelan <jeniduelan@gmail.com>
2024-04-25 19:16:46 +1000

    added the menu and choice function

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome to Epicreate")
+
+# Menu function
+def menu():
+    while True:
+        print("\nMain Menu:")
+        print("> Enter 1 to create a character ")
+        print("> Enter 2 to delete customised character ")
+        print("> Enter 3 to view customised character ")
+        print("> Enter 4 to exit ")
+
+        choice = input("Enter your choice: ")
+        return choice
+
+print("Exiting...\n")
