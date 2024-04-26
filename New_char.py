3dfa773206140e78da72f8e8c30d6defb31f0185
JEniduelan <jeniduelan@gmail.com>
2024-04-26 20:36:01 +1000

    added assign attributes

--- New_char.py
+++ New_char.py
@@ -6,6 +6,13 @@ class Character:
         "Mage": {"Strength": 3, "Dexterity": 5, "Intelligence": 10},
         "Rogue": {"Strength": 5, "Dexterity": 10, "Intelligence": 5}
     }
+    def __init__(self, name, race, char_class):
+        self.name = name
+        self.race = race
+        self.char_class = char_class
+        self.attributes = self.assign_attributes()
+        self.abilities = {}
+        self.assign_abilities()
 
 
 
