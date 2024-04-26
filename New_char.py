8dd90b77a2e8cf1508b1716348800d3778689ffd
JEniduelan <jeniduelan@gmail.com>
2024-04-26 20:45:48 +1000

    added abilites to character class

--- New_char.py
+++ New_char.py
@@ -1,11 +1,13 @@
 
-# Creating Character function
+# Character dictionary
 class Character:
+
     class_attributes = {
         "Warrior": {"Strength": 10, "Dexterity": 5, "Intelligence": 3},
         "Mage": {"Strength": 3, "Dexterity": 5, "Intelligence": 10},
         "Rogue": {"Strength": 5, "Dexterity": 10, "Intelligence": 5}
     }
+
     def __init__(self, name, race, char_class):
         self.name = name
         self.race = race
@@ -14,9 +16,19 @@ class Character:
         self.abilities = {}
         self.assign_abilities()
 
-
-
-
+    def assign_attributes(self):
+        return self.class_attributes.get(self.char_class, {})
+    
+    def assign_abilities(self):
+        if self.char_class == "Warrior":
+            self.abilities["Taunt"] = "Make nearby enemies to attack you"
+            self.abilities["Berserker Rage"] = "Makes the user deal additional damage and reduce incoming damage"
+        elif self.char_class == "Mage":
+            self.abilities["Fireball"] = "Launch a ball of fire at enemies"
+            self.abilities["Teleport"] = "Instantly teleport short distances"
+        elif self.char_class == "Rogue":
+            self.abilities["Backstab"] = "Sneak behind enemies and deal extra damage"
+            self.abilities["Stealth"] = "Become invisible temporarily"
 
 
 # Function to create a new character
